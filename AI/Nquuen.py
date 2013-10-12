7a2f5ef4e9f77cbe26b7a9e2361ee859415dba34
noah-lcf <feng.c.lee@gmail.com>
2013-10-12 17:12:44 +0800

    word2sql

--- AI/Nquuen.py
+++ AI/Nquuen.py
@@ -1,4 +1,5 @@
 # coding: UTF-8
+# 求解Nqueen 问题 算法练习
 __author__ = 'Noah'
 import numpy as np
 
@@ -33,13 +34,73 @@ def printPrettyBoard(board):
             print '\t\033[0m',
         print "\n"
 
-def findCells2Match(posX,posY):
-    cells=[]
+
+def findCells2MatchStraight(posX, posY):
+    cells_vertical = []
+    cells_horizontal = []
+    for i in range(0, 8):
+        if i != posX: cells_vertical.append([i, posY])
+        if i != posY: cells_horizontal.append([posX, i])
+    return cells_vertical, cells_horizontal
+
+
+def findCells2MatchLean(posX, posY):
+    '''
+    得到应该计算匹配的棋盘上的点,倾斜
+    '''
+    cells_left = []
+    cells_right = []
+    #左向
+    indexX = posX
+    indexY = posY
+    while indexX < 7 and indexY > 0:
+        indexX += 1
+        indexY -= 1
+        cells_left.append([indexX, indexY])
+    indexX = posX
+    indexY = posY
+    while indexX > 0 and indexY < 7:
+        indexX -= 1
+        indexY += 1
+        cells_left.append([indexX, indexY])
+        #右向
+    indexX = posX
+    indexY = posY
+    while indexX < 7 and indexY < 7:
+        indexX += 1
+        indexY += 1
+        cells_right.append([indexX, indexY])
+    indexX = posX
+    indexY = posY
+    while indexX > 0 and indexY > 0:
+        indexX -= 1
+        indexY -= 1
+        cells_right.append([indexX, indexY])
+    return cells_left, cells_right
+
+
+def getConfictCells(board):
+    matchCells = []
+    for columnIndex in range(0, len(board)):
+        for rowIndex in range(0, len(board[columnIndex])):
+            if board[columnIndex][rowIndex] == -1:
+                matchCell = [[rowIndex, columnIndex]]
+                cells_l, cells_r = findCells2MatchLean(rowIndex, columnIndex)
+                cells_v, cells_h = findCells2MatchStraight(rowIndex, columnIndex)
+                cells = cells_l + cells_r + cells_v + cells_h
+                for cell in cells:
+                    if (board[cell[1]][cell[0]]) == -1 and cell[1] > columnIndex: # 去掉右边判断，可以在find2Match里面进行判断直接不返回左边的
+                        matchCell += [[cell[0], cell[1]]]
+                if len(matchCell) > 1:
+                    matchCells.append(matchCell)
+    return matchCells
 
 
 if __name__ == '__main__':
     # testStat = np.arange(1, 9)
     # np.random.shuffle(testStat)
     testStat = [3, 6, 1, 7, 5, 4, 2, 8]
-    print testStat
-    print printPrettyBoard(getBoradStatus(testStat))
+    print "testStat:%s" % str(testStat)
+    boardStatus = getBoradStatus(testStat)
+    print printPrettyBoard(boardStatus)
+    print getConfictCells(boardStatus)
