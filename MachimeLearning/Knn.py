058c8be8f70ce6dd20c29800dbe6e325e4d7b255
noah-lcf <feng.c.lee@gmail.com>
2014-05-27 14:08:27 +0800

    add commont

--- MachimeLearning/Knn.py
+++ MachimeLearning/Knn.py
@@ -42,40 +42,47 @@ def file2matrix(filename):
 
 
 
-
+#归一化第一列数值，使之分布在0-1之间
 def autoNorm(dataSet):
+    print dataSet[:,0]
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
+    normDataSet = zeros(shape(dataSet))dataSet
     m = dataSet.shape[0]
+    #tile生成一个最小值矩阵和范围矩阵
     normDataSet = dataSet - tile(minVals, (m, 1))
+    #这里不是矩阵相除，特征值相除
     normDataSet = normDataSet / tile(ranges, (m, 1))   #element wise divide
+    print normDataSet[:,0]
     return normDataSet, ranges, minVals
 
 
 def datingClassTest():
     hoRatio = 0.50      #hold out 10%
     datingDataMat, datingLabels = file2matrix('../datingTestSet2.txt')       #load data setfrom file
+    # print datingDataMat[:0]
     normMat, ranges, minVals = autoNorm(datingDataMat)
     m = normMat.shape[0]
     numTestVecs = int(m * hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
         classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        # print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
         if (classifierResult != datingLabels[i]): errorCount += 1.0
     print "the total error rate is: %f" % (errorCount / float(numTestVecs))
     print errorCount
 
 
-# datingClassTest()
-group, label = createDataSet()
-print classify0([0.1,0.1],group, label, 1)
-# datingDataMat, datingLabels = file2matrix("../datingTestSet2.txt")
+datingClassTest()
+# group, label = createDataSet()
+# print classify0([0.1,0.1],group, label, 1)
+datingDataMat, datingLabels = file2matrix("../datingTestSet2.txt")
 # print datingDataMat
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
 # plt.show()
 # print(autoNorm(datingDataMat))
